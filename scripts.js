70862fc1e8b63fe3d40b06b1af4085f3787c9aee
Valentin Garcia <d3loygamer@gmail.com>
2022-06-15 12:00:27 -0300

    Commit 1

---
+++ scripts.js
@@ -0,0 +1,17 @@
+/*let numero = parseInt(prompt("Ingrese el numero de la tabla que desea ver"))
+
+for(let i=1; i<=10; i++){
+    let resultado = i*numero
+    console.log(numero + "X" + i + "=" + resultado)
+}
+*/
+
+/*
+for(let i = 1; i<=100; i++){
+    if( i % 2 === 0 ){
+        console.log(`El numero ${i} Es Par`)
+    } else{
+        console.log(`El numero ${i} Es Impar`)
+    }
+}
+*/
