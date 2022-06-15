2667d943cbbe350beea4dd0d3ba88164af55d0fb
Valentin Garcia <d3loygamer@gmail.com>
2022-06-15 12:03:52 -0300

    Commit 1

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
-/*let numero = parseInt(prompt("Ingrese el numero de la tabla que desea ver"))
+//EJEMPLO DE TABLA DE MULTIPLICAR
+
+let numero = parseInt(prompt("Ingrese el numero de la tabla que desea ver"))
 
 for(let i=1; i<=10; i++){
     let resultado = i*numero
     console.log(numero + "X" + i + "=" + resultado)
 }
-*/
 
-/*
+
+
+//CODIGO QUE MUESTRA EN CONSOLA UNA LISTA DE NUMEROS PARES E IMPARES HASTA EL 100
+
+
 for(let i = 1; i<=100; i++){
     if( i % 2 === 0 ){
         console.log(`El numero ${i} Es Par`)
@@ -14,4 +19,3 @@ for(let i = 1; i<=100; i++){
         console.log(`El numero ${i} Es Impar`)
     }
 }
-*/
