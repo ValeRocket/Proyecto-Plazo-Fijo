217121f9bb295f7a82e926a4f86bf8efa5776945
Valentin Garcia <d3loygamer@gmail.com>
2022-06-21 22:03:49 -0300

    Functions

--- scripts.js
+++ scripts.js
@@ -1,21 +1,24 @@
-//EJEMPLO DE TABLA DE MULTIPLICAR
+let capital = parseInt(prompt("Ingrese el monto"))
 
-let numero = parseInt(prompt("Ingrese el numero de la tabla que desea ver"))
-
-for(let i=1; i<=10; i++){
-    let resultado = i*numero
-    console.log(numero + "X" + i + "=" + resultado)
+function socialClub(){
+    alert("Su capital supera los 150,000$, accedera al SocialClub del Banco")
+        let datosSocialClub = (prompt("Ingrese nombre y apellido"))
+        alert("Bienvenido al SocialClub\n" + datosSocialClub )
 }
 
+function calculoPlazoFijo(numero1,numero2,numero3){
+   return numero1 * numero2 / numero3;
+}
 
+for(i=0; i <1 ;i++){
+    let operacion = capital*calculoPlazoFijo(0.3, 30, 365)
+    alert("La ganancia mensual de su plazo fijo es de\n" + "$" +parseInt(operacion) + "\nmensuales")
+    alert("Usted ahora posee"+ "\n$" + parseInt(operacion + capital))
 
-//CODIGO QUE MUESTRA EN CONSOLA UNA LISTA DE NUMEROS PARES E IMPARES HASTA EL 100
-
-
-for(let i = 1; i<=100; i++){
-    if( i % 2 === 0 ){
-        console.log(`El numero ${i} Es Par`)
-    } else{
-        console.log(`El numero ${i} Es Impar`)
+    
+    if(operacion <=150.000){
+        alert("Aun no puede ingresar al social club")
+    }else {
+        socialClub()
     }
 }
